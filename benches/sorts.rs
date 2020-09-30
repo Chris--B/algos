514852f524873b5315d60e0087a6d794df23a000
Chris Butler <chrisbutler296@gmail.com>
2020-09-30 01:49:32 -0400

    ⚡️ Run more benchmarks

--- benches/sorts.rs
+++ benches/sorts.rs
@@ -5,9 +5,37 @@ use criterion::Criterion;
 use criterion::Throughput;
 use criterion::{black_box, criterion_group, criterion_main};
 
-fn make_items(size: usize) -> Vec<i32> {
+/// Items in sorted order
+fn make_sorted_items(size: usize) -> Vec<i32> {
+    let size = size as i32;
+    (1..size).collect()
+}
+
+/// Items in reverse sorted order
+fn make_reverse_sorted_items(size: usize) -> Vec<i32> {
     let size = size as i32;
     (1..size).rev().collect()
+}
+
+/// Items in random order
+fn make_random_items(size: usize) -> Vec<i32> {
+    use rand::rngs::SmallRng;
+    use rand::seq::SliceRandom;
+    use rand::SeedableRng;
+
+    // Make sure we have the same items, we'll just shuffle the order
+    let mut all_items = make_sorted_items(size);
+
+    // We don't want this seed to change between runs, so it must never change between
+    // runs in the same process.
+    // It could change between independent runs, but currently does not.
+    let seed = u64::from_be_bytes(*b" #yolo !");
+
+    // Shuffle all of the items - this will produce the same sorted result as other runs.
+    let mut rng = SmallRng::seed_from_u64(seed);
+    all_items.shuffle(&mut rng);
+
+    all_items
 }
 
 fn do_sort_bench(
@@ -36,8 +64,8 @@ fn do_sort_bench(
     });
 }
 
-fn sorting_i32s(c: &mut Criterion) {
-    let mut group = c.benchmark_group("all sorts of sorts");
+fn sorting_with(c: &mut Criterion, name: &str, mut make_items: impl FnMut(usize) -> Vec<i32>) {
+    let mut group = c.benchmark_group(name);
 
     let items_set: Vec<(usize, Vec<i32>)> = [
         // 0, 1, 2, 3, 4, 5, 10, 100, 1_000, 2_000,
@@ -75,5 +103,23 @@ fn sorting_i32s(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, sorting_i32s);
+fn sorting_random_i32s(c: &mut Criterion) {
+    sorting_with(c, "n-random-items", make_random_items);
+}
+
+fn sorting_sorted_i32s(c: &mut Criterion) {
+    sorting_with(c, "n-already-sorted", make_sorted_items);
+}
+
+fn sorting_reverse_sorted_i32s(c: &mut Criterion) {
+    sorting_with(c, "n-reverse-sorted-items", make_reverse_sorted_items);
+}
+
+criterion_group!(
+    benches,
+    sorting_random_i32s,
+    sorting_sorted_i32s,
+    sorting_reverse_sorted_i32s
+);
+
 criterion_main!(benches);
