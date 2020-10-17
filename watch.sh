fc852035456cff1ff0e0c0fb5095a39248699f3b
Chris Butler <chrisbutler296@gmail.com>
2020-10-17 15:23:52 -0400

    🔨 Tidy `watch.sh`
    I haven't had cargo bench run in this in a while. It takes too long.
    
    A nice TODO would be to write a script that runs the benchmarks, and
    then packages the output data and graphs somewhere safe. I run
    `cargo clean` *way* too much to leave valuable data in `target/`.

--- watch.sh
+++ watch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cargo watch -c \
-    -x "check --workspace            " \
-    -x "fmt                          " \
-    -x "clippy --workspace           " \
-    -x "doc --document-private-items " \
-    -x "test                         " \
-    -x "bench                        " \
+cargo watch -c                            \
+    -x "check  --workspace              " \
+    -x "fmt                             " \
+    -x "clippy --workspace              " \
+    -x "doc    --document-private-items " \
+    -x "test   --workspace -q           "
